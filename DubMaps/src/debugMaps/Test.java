c0ad363ce91f531642c9166271598f72f2469a69
steveoaustin <steveoaustin812@gmail.com>
2016-08-23 14:56:11 -0700

    learning how to use git fml

--- DubMaps/src/debugMaps/Test.java
+++ DubMaps/src/debugMaps/Test.java
@@ -2,6 +2,6 @@ package debugMaps;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("This is a test");
+		System.out.println("This is a test\nme too thanks");
 	}
 }
