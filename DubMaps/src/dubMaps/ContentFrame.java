b241d2688fd16af85a726b4932c5fabf735451ac
steveoaustin <steveoaustin812@gmail.com>
2016-09-11 13:09:13 -0700

    display class complete with respect to default screen. contentFrame constructor and componentListeners added

--- DubMaps/src/dubMaps/ContentFrame.java
+++ DubMaps/src/dubMaps/ContentFrame.java
@@ -1,15 +1,87 @@
 package dubMaps;
 
+import java.awt.Dimension;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
+
+/*
+ * ContentFrame is the parent component for map GUIs. It holds a contentPanel containing all
+ * child components, and restricts resizing to reasonable boundaries
+ */
 public class ContentFrame extends JFrame {
+	
+	private static final double MIN_SCREEN_RATIO = 0.3;
+	private Dimension minSize, maxSize;
 
 	public ContentFrame() {
+		// add GUI content
 		ContentPanel content = new ContentPanel();
 		add(content);
-		setVisible(true);
+		
+		// set frame properties
+		setSizeRestrictions();
 		setResizable(true);
+		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		
+		// add event listeners
+		addComponentListener(new ComponentListener() {
+			/**
+			 * Recalculates the frame's max and min size in case it was moved to a new window
+			 * in a multiple monitor display
+			 * @param e: Component event associated with resizing
+			 */
+			@Override
+			public void componentMoved(ComponentEvent e) {
+				setSizeRestrictions();
+			}
+			
+			@Override
+			public void componentResized(ComponentEvent e) { /* ignore */ }	
+			
+			@Override
+			public void componentShown(ComponentEvent e) {	/* ignore */ }
+			
+			@Override
+			public void componentHidden(ComponentEvent e) {	/* ignore */ }			
+		});
 	}
+	
+	/*
+	 * Gets current display window dimensions and uses them to restrict frame size
+	 */
+	private void setSizeRestrictions() {
+		int min = (int) (Display.getWidth() * MIN_SCREEN_RATIO);
+		minSize = new Dimension(min, min);
+		maxSize = new Dimension(Display.getWidth(), Display.getHeight());
+		setMaximumSize(maxSize);
+		setMinimumSize(minSize);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- DubMaps/src/dubMaps/Display.java
+++ DubMaps/src/dubMaps/Display.java
@@ -1,8 +1,59 @@
 package dubMaps;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+/*
+ * Display mimics static class behavior to be used by all GUI components that need 
+ * information about the current screen dimensions. Holds screen width and height information,
+ * and the maximum and minimum scaling ratios for GUI images
+ */
 public final class Display {
 
-	private Display() {
-		
+	// scale ratios in the form of screen pixels to image pixels
+	private static final double MIN_SCALE_RATIO = 1.0, MAX_SCALE_RATIO = 5.0;
+	private static GraphicsDevice screen = 
+			GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+	private static int screenWidth = screen.getDisplayMode().getWidth();
+	private static int screenHeight = screen.getDisplayMode().getHeight();
+	
+	// private constructor mimics static class behavior
+	private Display() {}
+	
+	/**
+	 * returns the maximum scale ratio
+	 * @return MAX_SCALE_RATIO: the max ratio for screen pixels to image pixels
+	 */
+	public static double getMaxRatio() {
+		return MAX_SCALE_RATIO;
 	}
+	
+	/**
+	 * returns the minimum scale ratio
+	 * @return MIN_SCALE_RATIO: the min ratio for screen pixels to image pixels
+	 */
+	public static double getMinRatio() {
+		return MIN_SCALE_RATIO;
+	}
+	
+	/**
+	 * updates and returns the current screen's width
+	 * @return screen width as an integer
+	 * TODO: get width of the current screen, not default
+	 */
+	public static int getWidth() {	
+		screenWidth = screen.getDisplayMode().getWidth();
+		return (int) screenWidth;
+	}
+	
+	/**
+	 * updates and returns the current screen's height
+	 * @return screen height as an integer
+	 * TODO: get height of the current screen, not default
+	 */
+	public static int getHeight() {
+		screenHeight = screen.getDisplayMode().getHeight();
+		return (int) screenHeight;
+	}
+	
 }
