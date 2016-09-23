412078d4b2c5b07670ef91dada445922f2ba757c
steveoaustin <steveoaustin812@gmail.com>
2016-09-23 12:27:34 -0700

    Style an d comment updates to mapPanel

--- DubMaps/src/navigator/MapPanel.java
+++ DubMaps/src/navigator/MapPanel.java
@@ -55,6 +55,151 @@ public class MapPanel extends JPanel {
 		closestEntrance = new int[2];
 		repaint();
 		setVisible(true);
+	}	
+	
+	/**
+	 * Handles left click events in the parent container
+	 * @param x: Mouse's x coordinate
+	 * @param y: Mouse's y coordinate
+	 */
+	public boolean leftClick(int x, int y) {
+		return handlePath(x, y);
+	}
+	
+	/**
+	 * Handles right click events in the parent container
+	 * @param x: Mouse's x coordinate
+	 * @param y: Mouse's y coordinate
+	 */
+	public boolean rightClick(int x, int y) {
+		return clearPath();
+	}
+	
+	/**
+	 * Handles mouse moved events from the parent container
+	 * @param x: Mouse's x coordinate
+	 * @param y: Mouse's y coordinate
+	 */
+	public void mouseMoved(int x, int y) {
+		highlightClosestBuilding(x, y);
+	}
+	
+	/**
+	 * Update display to accommodate current window bounds
+	 * @param width: Width of the window
+	 * @param height: Height of the window
+	 */
+	public void updateDisplay(int width, int height) {
+		if (path) {
+			Rectangle bounds = ui.getPathBounds(model.getPath(pathStart, pathDest));
+			map.zoomIn(parent.getWidth(), parent.getHeight(),
+					new Dimension(bounds.width, bounds.height));
+			setScrollCenter();
+		} else {
+			map.zoomOut(width, height);
+		}
+		setPreferredSize(new Dimension(map.getWidth(), map.getHeight()));
+		repaint();
+		parent.scrollToCenter();
+	}
+	
+	/**
+	 * Sets the parent scroll pane's scroll center-point to the center of
+	 * the drawn path
+	 */
+	public void setScrollCenter() {
+		// exit if a path is not drawn
+		if (pathStart == null || pathDest == null) { return; } 
+		Rectangle bounds = ui.getPathBounds(model.getPath(pathStart, pathDest));
+		
+		// calculate the "percentage" that scrollBars should be set to, scaling 
+		// with respect to native image pixels
+		double x = ((bounds.x + (1.0 * bounds.width / 2)) / map.getMap().getWidth());
+		double y = ((bounds.y + (1.0 * bounds.height / 2)) / map.getMap().getHeight());
+		parent.setCenter(x, y);
+	}
+	
+	/**
+	 * Highlights the closest building to the x y coordinate
+	 * @param x: X coordinate
+	 * @param y: Y coordinate
+	 */
+	public void highlightClosestBuilding(int x, int y) {
+		if (path) { return; } // don't highlight entrances if a path is already drawn
+		x *= ui.scaleWidth();
+		y *= ui.scaleHeight();
+		Node<Location> n = model.getClosestBuilding(x, y, MAX_DISTANCE);
+		// quit if a the mouse if too far from a building, reset closestEntrance
+		if (n == null) { 
+			closestEntrance[0] = -1;
+			closestEntrance[1] = -1;
+			repaint();
+			return; 
+		}  
+		
+		// scale the coordinates of n to fit on screen
+		List<Location> temp = new ArrayList<Location>();
+		temp.add(n.getLocation());
+		int[] loc = ui.getBuildingEntrances(temp).get(0);
+		closestEntrance[0] = loc[0];
+		closestEntrance[1] = loc[1];
+		repaint();
+	}
+	
+	/**
+	 * Handles the path drawing behavior of mapPanel, and returns a value indicating 
+	 * whether the path was updated
+	 * @param x: The click's x coordinate
+	 * @param y: The clicks's y coordinate
+	 * @return true if a path was drawn, false otherwise
+	 */
+	public boolean handlePath(int x, int y) {
+		if (path) { return false; } // exit if path is already drawn
+		// Convert click locations to image pixels
+		x *= ui.scaleWidth();
+		y *= ui.scaleHeight();
+		Node<Location> n = model.getClosestBuilding(x, y, MAX_DISTANCE);
+		if (n == null) { return false; }  // quit if a valid node is not found
+		
+		if (pathStart == null) {
+			pathStart = n;
+			repaint();
+			return false;
+		} else if (pathStart != null && pathStart != n) {
+			pathDest = n;
+			path = true;
+			updateDisplay(parent.getWidth(), parent.getHeight());
+			parent.scrollToCenter();
+		}
+		return true;
+	}
+	
+	/**
+	 * Resets path data in mapPanel; returns true if a drawn path is cleared
+	 * @return
+	 */
+	public boolean clearPath() {
+		// clear path 
+		pathStart = null;
+		pathDest = null;
+		parent.resetCenter();		
+		// only re-scroll to center if a path was drawn
+		if (path) {
+			path = false;
+			updateDisplay(parent.getWidth(), parent.getHeight());
+			return true;
+		}
+		repaint();
+		return false;
+	}
+	
+	/**
+	 * Sets parent to the mapScrollPane holding this map. parent must be set to enable
+	 * scrolling features
+	 * @param parent: The parent mapScrollPane
+	 */
+	public void setParent(MapScrollPane parent) {
+		this.parent = parent;
 	}
 	
 	/**
@@ -133,132 +278,5 @@ public class MapPanel extends JPanel {
 			g2d.setColor(Color.red);
 			g2d.drawString(s[0], x, y);
 		}	
-	}
-	
-	
-	public boolean leftClick(int x, int y) {
-		return handlePath(x, y);
-	}
-	
-	public boolean rightClick(int x, int y) {
-		return clearPath();
-	}
-	
-	public void mouseMoved(int x, int y) {
-		highlightClosestBuilding(x, y);
-	}
-	
-	/**
-	 * Update display to accommodate current window bounds
-	 * @param width: Width of the window
-	 * @param height: Height of the window
-	 */
-	public void updateDisplay(int width, int height) {
-		if (path) {
-			Rectangle bounds = ui.getPathBounds(model.getPath(pathStart, pathDest));
-			map.zoomIn(parent.getWidth(), parent.getHeight(),
-					new Dimension(bounds.width, bounds.height));
-			setScrollCenter();
-		} else {
-			map.zoomOut(width, height);
-		}
-		setPreferredSize(new Dimension(map.getWidth(), map.getHeight()));
-		repaint();
-		parent.scrollToCenter();
-	}
-	
-	/**
-	 * sets the parent scroll pane's scroll center-point to the center of
-	 * the drawn path
-	 */
-	public void setScrollCenter() {
-		// exit if a path is not drawn
-		if (pathStart == null || pathDest == null) { return; } 
-		Rectangle bounds = ui.getPathBounds(model.getPath(pathStart, pathDest));
-		
-		// calculate the "percentage" that scrollBars should be set to, scaling 
-		// with respect to native image pixels
-		double x = ((bounds.x + (1.0 * bounds.width / 2)) / map.getMap().getWidth());
-		double y = ((bounds.y + (1.0 * bounds.height / 2)) / map.getMap().getHeight());
-		parent.setCenter(x, y);
-	}
-	
-	
-	public void highlightClosestBuilding(int x, int y) {
-		if (path) { return; } // don't highlight entrances if a path is already drawn
-		x *= ui.scaleWidth();
-		y *= ui.scaleHeight();
-		Node<Location> n = model.getClosestBuilding(x, y, MAX_DISTANCE);
-		// quit if a the mouse if too far from a building, reset closestEntrance
-		if (n == null) { 
-			closestEntrance[0] = -1;
-			closestEntrance[1] = -1;
-			repaint();
-			return; 
-		}  
-		
-		// scale the coordinates of n to fit on screen
-		List<Location> temp = new ArrayList<Location>();
-		temp.add(n.getLocation());
-		int[] loc = ui.getBuildingEntrances(temp).get(0);
-		closestEntrance[0] = loc[0];
-		closestEntrance[1] = loc[1];
-		repaint();
-	}
-	
-	/**
-	 * Handles the path drawing behavior of mapPanel, and returns a value indicating 
-	 * whether the path was updated
-	 * @param x: The click's x coordinate
-	 * @param y: The clicks's y coordinate
-	 * @return true if a path was drawn, false otherwise
-	 */
-	public boolean handlePath(int x, int y) {
-		if (path) { return false; } // exit if path is already drawn
-		// Convert click locations to image pixels
-		x *= ui.scaleWidth();
-		y *= ui.scaleHeight();
-		Node<Location> n = model.getClosestBuilding(x, y, MAX_DISTANCE);
-		if (n == null) { return false; }  // quit if a valid node is not found
-		
-		if (pathStart == null) {
-			pathStart = n;
-			repaint();
-			return false;
-		} else if (pathStart != null && pathStart != n) {
-			pathDest = n;
-			path = true;
-			updateDisplay(parent.getWidth(), parent.getHeight());
-			parent.scrollToCenter();
-		}
-		return true;
-	}
-	
-	/**
-	 * Resets path data in mapPanel; returns true if a drawn path is cleared
-	 * @return
-	 */
-	public boolean clearPath() {
-		// clear path 
-		pathStart = null;
-		pathDest = null;
-		parent.resetCenter();		
-		// only re-scroll to center if a path was drawn
-		if (path) {
-			path = false;
-			updateDisplay(parent.getWidth(), parent.getHeight());
-			return true;
-		}
-		repaint();
-		return false;
-	}
-	
-	/**
-	 * Sets parent to the mapScrollPane holding this map. parent must be set to enable
-	 * scrolling features
-	 * @param parent: The parent mapScrollPane
-	 */
-	public void setParent(MapScrollPane parent) {
-		this.parent = parent;
 	}
 }
